5ae2ebd6510251263cd6a0563f53fb556d812c50
trucquynhpct <108359460+trucquynhpct@users.noreply.github.com>
2023-02-21 10:06:33 +0700

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+import { Navbar, NavbarBrand } from 'reactstrap';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
-export default App;
