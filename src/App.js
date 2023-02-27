dfd4f722075cc4751b12e0f68fdd8edd077369eb
trucquynhpct <108359460+trucquynhpct@users.noreply.github.com>
2023-02-27 22:30:07 +0700

    Functional Components

--- src/App.js
+++ src/App.js
@@ -3,28 +3,20 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import './App.css';
+import Main from './components/MainComponent';
 
 class App extends Component {
-   constructor(props) {
-      super(props);
-      
-      this.state = {
-         dishes: DISHES
-      }
-   }
-  
-   render() {  
-      return (
-         <div>
-            <Navbar dark color="primary">
-               <div className="container">
-                  <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-               </div>
-            </Navbar>
-           <Menu dishes={this.state.dishes} />
-        </div>
+
+
+   render() {
+     return (
+       <div className="App">
+         <Main />
+       </div>
      );
    }
-}
+ }
+ 
+
 
 export default App;
